4c3199953473df66ca66ecddd94d4dba4a0d3f12
Andrei <catrina.andrei93@yahoo.com>
2023-06-03 22:47:26 +0300

    Header implemented

---
+++ app/components/Header/Header.jsx
@@ -0,0 +1,61 @@
+"use client";
+import React from "react";
+
+import {
+  AboutLink,
+  ContactLink,
+  DonateLink,
+  HeaderContainer,
+  HeaderSection,
+  LogoContainer,
+  MenuContainer,
+  SubtitleContainer,
+  Title,
+  TitleContainer,
+} from "./components";
+
+import Image from "next/image";
+import Logo from "../../../public/logo1.png";
+import Banner from "../../../public/Banner.jpg";
+import { FaDonate } from "react-icons/fa";
+import { BsFillTelephoneFill } from "react-icons/bs";
+
+export const Header = () => {
+  return (
+    <HeaderSection>
+      <Image
+        src={Banner}
+        style={{
+          position: "absolute",
+          top: "0",
+          left: "0",
+          width: "100%",
+          height: "100%",
+          objectFit: "cover",
+          // filter: "blur(1px)",
+        }}
+      />
+
+      <HeaderContainer>
+        <LogoContainer>
+          {/* <Image src={Logo} alt="" /> */}
+          Logo
+        </LogoContainer>
+        <MenuContainer>
+          <DonateLink href="">
+            Doneaza <FaDonate />
+          </DonateLink>
+          <AboutLink href="">Despre Noi</AboutLink>
+          <ContactLink href="">
+            Contact <BsFillTelephoneFill />
+          </ContactLink>
+        </MenuContainer>
+      </HeaderContainer>
+      <TitleContainer>
+        <Title>ÎMPREUNĂ</Title>
+        <span>CONSTRUIM</span>
+        <p>O LUME MAI BUNĂ</p>
+      </TitleContainer>
+    </HeaderSection>
+  );
+};
---
+++ app/components/Header/components.jsx
@@ -0,0 +1,189 @@
+import styled from "styled-components";
+
+export const HeaderSection = styled.section`
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  height: 500px;
+
+  position: relative;
+
+  img {
+    /* width: 100%;
+    height: 100%;
+    object-fit: cover;
+    position: absolute;
+    top: 0;
+    left: 0; */
+  }
+`;
+
+export const HeaderContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  /* align-items: center; */
+  width: 100%;
+  max-width: 1600px;
+  padding: 12px 24px;
+`;
+
+export const LogoContainer = styled.div`
+  display: flex;
+  z-index: 1;
+  font-size: 24px;
+  font-weight: 700;
+
+  img {
+    width: 240px;
+    height: 160px;
+  }
+`;
+
+export const MenuContainer = styled.div`
+  display: flex;
+  gap: 20px;
+  z-index: 1;
+
+  a {
+    height: 40px;
+    padding: 0 12px;
+  }
+`;
+
+export const DonateLink = styled.a`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 4px;
+  border: 1px solid transparent;
+  font-size: 16px;
+  font-weight: 400;
+  background-color: #116530;
+  color: white;
+  border-radius: 6px;
+  transition: 0.6s;
+
+  svg {
+    color: #ecf87f;
+    transition: 0.6s;
+  }
+
+  :hover {
+    background-color: transparent;
+    border: 1px solid #116530;
+    color: #116530;
+    transform: scale(1.1);
+
+    svg {
+      color: #116530;
+    }
+  }
+`;
+
+export const AboutLink = styled.a`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: 1px solid #116530;
+  border-radius: 6px;
+  font-size: 16px;
+  font-weight: 500;
+  transition: 0.6s;
+
+  :hover {
+    background-color: #116530;
+    color: white;
+    transform: scale(1.1);
+  }
+`;
+
+export const ContactLink = styled.a`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 4px;
+  border: 1px solid #116530;
+  border-radius: 6px;
+  font-size: 16px;
+  font-weight: 500;
+  transition: 0.6s;
+
+  svg {
+    transition: 0.6s;
+  }
+
+  :hover {
+    background-color: #116530;
+    color: white;
+    transform: scale(1.1);
+
+    svg {
+      color: white;
+    }
+  }
+`;
+
+export const TitleContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 12px;
+  padding: 8px 0;
+  position: absolute;
+  bottom: 150px;
+  left: 0;
+  width: 100%;
+  /* max-width: 400px; */
+
+  border-radius: 8px;
+
+  span {
+    display: flex;
+    justify-content: center;
+    font-size: 40px;
+    font-weight: 500;
+    color: black;
+    width: 100%;
+    background: rgba(255, 255, 255, 0.1);
+    -webkit-backdrop-filter: blur(1px);
+    backdrop-filter: blur(1px);
+    border: 1px solid rgba(255, 255, 255, 0.05);
+    animation: fadeIn 6s;
+  }
+
+  p {
+    font-size: 32px;
+    color: white;
+    animation: fadeIn 12s;
+    font-weight: 500;
+  }
+
+  @keyframes fadeIn {
+    0% {
+      opacity: 0;
+    }
+
+    100% {
+      opacity: 1;
+    }
+  }
+`;
+
+export const Title = styled.h3`
+  font-size: 64px;
+  font-weight: 500;
+  color: white;
+  animation: fadeIn 5s;
+
+  @keyframes fadeIn {
+    0% {
+      opacity: 0;
+    }
+
+    100% {
+      opacity: 1;
+    }
+  }
+`;
+
+export const SubtitleContainer = styled.div``;
